e71f04622db9fa9823dc972c46fdbf520deb1436
Yoko Li <yokolee456@gmail.com>
2020-10-03 23:34:29 +0800

    Add CmdCreateStudent and exceptions

---
+++ EventManagementSystem/ExInsufficientArguments.java
@@ -0,0 +1,6 @@
+package EventManagementSystem;
+
+public class ExInsufficientArguments extends Exception
+{
+    public ExInsufficientArguments() { super("Insufficient command arguments!"); }
+}
---
+++ EventManagementSystem/ExStudentIDtooLong.java
@@ -0,0 +1,5 @@
+package EventManagementSystem;
+
+public class ExStudentIDtooLong extends Exception{
+    public ExStudentIDtooLong() { super("StudentID cannot exceed 8 characters!"); }
+}
