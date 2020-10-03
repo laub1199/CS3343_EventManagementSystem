02f408f5d612fa6eb177f4bc64c4cfee08f15368
Yoko Li <yokolee456@gmail.com>
2020-10-04 00:18:09 +0800

    Add CmdDeleteStudent

--- EventManagementSystem/CmdDeleteStudent.java
+++ EventManagementSystem/CmdDeleteStudent.java
@@ -3,6 +3,16 @@ package EventManagementSystem;
 public class CmdDeleteStudent implements Command {
     @Override
     public void execute(String[] cmdParts) throws CloneNotSupportedException {
-
+        try {
+            if (cmdParts.length < 3) {
+                throw new ExInsufficientArguments();
+            }
+            StudentHandler s = StudentHandler.getInstance();
+            s.deleteStudent(cmdParts[3]);
+        } catch (NullPointerException e) {
+            System.out.println("Wrong Student ID!");
+        } catch (ExInsufficientArguments e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
