6996f28f9eb1f97237e0c231e9631ce68f37aa4b
Sennett Lau <laub1199@gmail.com>
2020-10-08 01:16:59 +0800

    added cmdDeleteGroup

--- EventManagementSystem/CmdDeleteGroup.java
+++ EventManagementSystem/CmdDeleteGroup.java
@@ -3,6 +3,14 @@ package EventManagementSystem;
 public class CmdDeleteGroup implements Command {
     @Override
     public void execute(String[] cmdParts) throws CloneNotSupportedException {
-
+        try {
+            if (cmdParts.length != 3) {
+                throw new ExWrongCommand();
+            }
+            GroupHandler instance = GroupHandler.getInstance();
+            instance.deleteGroup(cmdParts[2]);
+        } catch (ExWrongCommand | ExGroupNotFound e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
